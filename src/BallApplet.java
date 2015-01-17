0e9d985eb3789821f61e995ab52a86060112f203
Mohamed Refaat <eltntawy@gmail.com>
2015-01-18 00:10:58 +0200

    test commit

--- src/BallApplet.java
+++ src/BallApplet.java
@@ -5,7 +5,9 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.*;
 import java.awt.event.* ;
-
+/*
+ * this is class for ball applet demo
+ * */
 public class BallApplet extends Applet {
 
 	Image image;
---
+++ src/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author eltntawy
+ * 
+ */
+public class Test {
+
+   
+    /**
+     * @param s
+     * @param d : is the input mentod 2
+     */
+    public void method(String s,String d) {
+	
+    }
+    
+}
